f5a61f0dbfe3b36bcea1d70c176f6f6f11e00000
asdwhy <mail@joshuacarrasco.com>
2022-03-12 20:45:11 -0500

    updated docs2

--- src/app/pages/documents/documents.component.ts
+++ src/app/pages/documents/documents.component.ts
@@ -8,10 +8,6 @@ import { Router } from '@angular/router';
 })
 export class DocumentsComponent implements OnInit {
 
-  constructor(
-    private routerService: Router
-  ) { }
-
   ngOnInit(): void {
   }
 
