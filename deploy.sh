e0ce8c852113609073acfcb234e9d1966b874d3c
Joshua Carrasco <mail@joshuacarrasco.com>
2024-12-21 19:36:41 -0500

    updated email

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,6 @@
 #!/usr/bin/env sh
 
 rm -r public
-hugo && rsync -avz --delete public/ jecs-ovh:~/webroot/joshuacarrasco.com
+hugo
+rsync -avz --delete public/ jecs-ovh:~/webroot/joshuacarrasco.com
+rsync -avz --delete public/ jecs-ovh:~/webroot/jecs.me
